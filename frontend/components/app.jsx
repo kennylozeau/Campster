5a1c1cf933444a9cfa29987f5c82deba1a68cc0e
kenny <kenny.lozeau@gmail.com>
2019-12-09 19:46:18 -0500

    Create Test component for testing FE routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import Test from './test';
 
 const App = (props) => {
   return(
-    <h1>React is working!!!</h1>
+    <>
+      <h1>React is working!!!</h1>
+      <Route path="/login" component={Test} />
+    </>
   );
 };
 
