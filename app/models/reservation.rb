e1d4a4730d563479aa5cca27b0ece4a804c4742e
kenny <kenny.lozeau@gmail.com>
2019-12-13 16:16:00 -0500

    Add basic associations for reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: reservations
+#
+#  id          :bigint           not null, primary key
+#  campsite_id :integer          not null
+#  camper_id   :integer          not null
+#  start_date  :date             not null
+#  end_date    :date             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Reservation < ApplicationRecord
+
+  validates :campsite_id, :camper_id, :start_date, :end_date, presence: true
+
+  belongs_to :campsite,
+    class_name: :Campsite,
+    primary_key: :id,
+    foreign_key: :campsite_id
+
 end
