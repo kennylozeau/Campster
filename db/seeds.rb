13dfb485438d875d9eb4b11149b30a5875932d4b
kenny <kenny.lozeau@gmail.com>
2019-12-13 10:46:56 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Campsite.delete_all
+Campsite.destroy_all
+User.destroy_all
 
 camp1 = Campsite.create!(
   name: "Brewster River Camping",
@@ -24,4 +25,30 @@ camp2 = Campsite.create!(
   longitude: 114.775278,
   location: "Lozeau, Montana",
   price: 17.50
+)
+
+camp3 = Campsite.create!(
+  name: "Forest Lake Reserve",
+  description: "Get lost in the wilderness that surrounds you!",
+  latitude: 43.533547,
+  longitude: -72.141985,
+  location: "Grantham, New Hampshire",
+  price: 1.0
+)
+
+camp4 = Campsite.create!(
+  name: "Summit Overlook Park",
+  description: "Beautiful views of peaks in the distance",
+  latitude: 41.267211,
+  longitude: -123.148286,
+  location: "Sawyers Bar, California",
+  price: 12.0
+)
+
+demo_user = User.create!(
+  email: "kenny@kenny.com",
+  first_name: "Kenny",
+  last_name: "Lozeau",
+  zip_code: 11237,
+  password: "123456"
 )
