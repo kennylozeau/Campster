c4fe14b226219ca2d10ba285a910c38f47ae21c1
kenny <kenny.lozeau@gmail.com>
2019-12-17 15:55:13 -0500

    Set uglifier gem to use harmony in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.assets.uglifier = { harmony: true }
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
